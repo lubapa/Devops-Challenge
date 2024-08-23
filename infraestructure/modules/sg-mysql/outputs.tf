88e65f047443ce6f0b34fb6232823a38ab2fae3d
Luis Bahamondes <l_bahamondes@hotmail.com>
2024-08-22 21:47:12 -0400

    clear folders

--- infraestructure/modules/sg-mysql/outputs.tf
+++
@@ -1,6 +0,0 @@
-output "vpc_id" {
-  value = aws_security_group.mysql_sg.vpc_id
-}
-output "security_group_id" {
-  value = aws_security_group.mysql_sg.id
-}
--- infraestructure/modules/sg-mysql/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "name" {
-  description = "Name of the Security Group"
-  type        = string
-}
-variable "vpc_id" {
-  description = "ID of the VPC to create security group"
-  type        = string
-}
